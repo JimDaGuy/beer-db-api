e42b76d76c422d128c10d9d5c07ac2426a711cf5
James Digrazia <jtd2401@rit.edu>
2019-03-13 09:15:08 -0400

    Implement routes for creating and retrieving comments

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const Grid = require("gridfs-stream");
 
 const router = app => {
   /*
-  findRecipeByName
+  GET findRecipeByName
   - Case-insensitive search to name value of Recipes collection
   =========================
   Parameters:
@@ -46,7 +46,7 @@ const router = app => {
   });
 
   /*
-  image
+  GET image
   - Return image of beer baed on the color parameter passed in
   ==========================
   Parameters:
@@ -95,6 +95,84 @@ const router = app => {
           readStream.pipe(res);
           break;
       }
+    });
+  });
+
+  /*
+  GET comments
+  - Return comments for a beer document with matching beer id
+  ==================
+  Parameters:
+  beerID (Required): ID of beer to retrieve comments for
+  rpp (Default: 5): Results per page
+  page (Default: 1): Page of results - 1 indexed
+  */
+  app.get("/comments", (req, res) => {
+    // Check if beerID parameter is given
+    if (!req.query.beerID) {
+      return res.status(400).json({ error: "beerID parameter is required" });
+    }
+
+    // Local variables from parameters
+    const { beerID } = req.query;
+    const rpp = parseInt(req.query.rpp) || 5;
+    const page = parseInt(req.query.page) || 1;
+
+    // Connect to database
+    MongoClient.connect(dbURL, (err, database) => {
+      if (err) throw err;
+      // Get database object
+      const dbObject = database.db(dbName);
+
+      // Search Comments collection
+      dbObject
+        .collection("Comments")
+        .find({ beerID }, {})
+        .sort({ _id: -1 })
+        .skip(rpp * (page - 1))
+        .limit(rpp)
+        .toArray((err2, result) => {
+          if (err2) throw err2;
+
+          res.status(200).send(result);
+        });
+    });
+  });
+
+  /**
+   POST comment 
+   - Create comment for a beer document with given beerID
+   ==================
+   Parameters:
+   beerID (Required): ID of beer to associate with comment
+   comment (Required): Comment to associate with beerID
+   */
+  app.post("/comment", (req, res) => {
+    // Check if beerID and comment parameters are given
+    if (!req.query.beerID || !req.query.comment) {
+      return res
+        .status(400)
+        .json({ error: "beerID and comment parameters are required" });
+    }
+
+    const { beerID, comment } = req.query;
+
+    // Connect to database
+    MongoClient.connect(dbURL, (err, database) => {
+      if (err) throw err;
+      // Get database object
+      const dbObject = database.db(dbName);
+
+      // Post comment to Comments collection
+      dbObject
+        .collection("Comments")
+        .insertOne({ beerID, comment }, (err2, result) => {
+          if (err2) throw err2;
+
+          res
+            .status(200)
+            .send(`Comment for beerID: ${beerID} was successfully saved`);
+        });
     });
   });
 };
